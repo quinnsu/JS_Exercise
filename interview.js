06356282fb052cd65dfe37835073e35d3ab2f3b0
悄兮 <2509253965@qq.com>
2025-01-04 22:20:24 +0800

    深拷贝浅拷贝

--- interview.js
+++ interview.js
@@ -45,4 +45,19 @@ function deepCopy(newObj,oldObj){
     for (let key in oldObj){
         let item = oldObj[key]
     }
-}
+    if(item instanceof Array){
+        newObj[key] = [];
+        deepCopy(newObj[key],item);}
+    else if (item instanceof Object){
+        newObj[key]={};
+        deepCopy(newObj[key],item);
+    }
+    else{
+        newObj[key] = item;
+    }
+    }
+
+
+
+
+    //T2. 类型判断
